526f3776cdecd227f19d7e052bc965fa68a4d533
chaou <schaouipro21@gmail.com>
2023-05-15 15:21:04 +0200

    classes et fichiers fxml

--- src/main/java/com/example/harry_said_chaoui_fx/controllers/CharController.java
+++ src/main/java/com/example/harry_said_chaoui_fx/controllers/CharController.java
@@ -1,13 +1,9 @@
 package com.example.harry_said_chaoui_fx.controllers;
 
-import com.example.harry_said_chaoui_fx.HelloApplication;
+import com.example.Game;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -18,12 +14,13 @@ public class CharController {
 
     @FXML
     protected void submitName(ActionEvent actionEvent) throws IOException {
+        Game game = new Game();
+        game.startGame();
+
         String name = nameField.getText();
         System.out.println(name);
 
 
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("game-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        ((Stage) ((Button) actionEvent.getSource()).getScene().getWindow()).setScene(scene);
+
     }
 }
--- src/main/java/com/example/harry_said_chaoui_fx/controllers/StartController.java
+++ src/main/java/com/example/harry_said_chaoui_fx/controllers/StartController.java
@@ -1,5 +1,6 @@
 package com.example.harry_said_chaoui_fx.controllers;
 
+import com.example.Game;
 import com.example.harry_said_chaoui_fx.HelloApplication;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,6 +15,7 @@ public class StartController {
 
     @FXML
     protected void onStartGameButtonClick(ActionEvent actionEvent) throws IOException {
+
 
         // Chargement de la nouvelle scène
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("char-view.fxml"));
